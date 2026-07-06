b05f3aa42a71f842dc14e8b9e346edb53bf18f9a
daniel@localhost <daniel@localhost>
2026-07-05 20:18:18 -0400

    fix naming buch in prepare-ligand_hydration.sh script

--- hydration/prepare-ligand_hydration.sh
+++ hydration/prepare-ligand_hydration.sh
@@ -3,4 +3,4 @@ scrub.py "$1" -o "${1%.sdf}_H.sdf" --skip_tautomers --ph_low 7.35 --ph_high 7.45
 
 
 # make pdbqt with -w option so that waters are added to ligand
-mk_prepare_ligand.py -w -i "${1%.sdf}_H.sdf" -o "${1%.sdf}_H.pdbqt"$1.sdf 
+mk_prepare_ligand.py -w -i "${1%.sdf}_H.sdf" -o "${1%.sdf}_H.pdbqt" 
